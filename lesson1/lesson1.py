dd65b507b5547775a2daa8ce3fb4afbc20171fec
2pokek <alkuzmich6479@gmail.com>
2022-12-02 01:14:14 +0300

    lesson 1.2

---
+++ lesson1/lesson1.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+
+with open("blank/index.html") as file:
+    src = file.read()
+
+soup = BeautifulSoup(src, 'lxml')
+
+title = soup.title
+print(title)
+print(title.text)
+print(title.string)
+
+# .find .find_all
+page_h1 = soup.find("h1")
+print(page_h1)
+
+page_all_h1 = soup.find_all("h1")
+print(page_all_h1)
+
+for item in page_all_h1:
+    print(item.text)
+
+# user_name = soup.find('div', class_='user__name')
+# print(user_name.text.strip())
+#
+# user_name = soup.find('div', class_='user__name').find("span").text
+# print(user_name)
+#
+# user_name = soup.find("div", {"class": "user__name", "id": "aaa"}).find("span").text
+# print(user_name)
+
+find_all_spans_in_user_info = soup.find(class_='user__info').find_all('span')
+print(find_all_spans_in_user_info)
+
+# for item in find_all_spans_in_user_info:
+#     print(item.text)
+
+print(find_all_spans_in_user_info[0])
+print(find_all_spans_in_user_info[2].text)
+
+social_links = soup.find(class_='social__networks').find('url').find_all('a')
+print(social_links)
+
+all_a = soup.find_all('a')
+print(all_a)
+
+for item in all_a:
+    item_text = item.text
+    item_url = item.get('href')
+    print(f'{item_text}:{item_url}')
