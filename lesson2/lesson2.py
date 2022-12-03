67b5a4e2afa7ea44e5f67f1a33ac625845532a34
2pokek <alkuzmich6479@gmail.com>
2022-12-03 16:17:19 +0300

    lesson 2.2

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -15,5 +15,19 @@ import csv
 # src = req.text
 # print(src)
 
-with open('index.html','w') as file:
-    file.write(src)
+with open('index.html') as file:
+    src = file.read()
+
+soup = BeautifulSoup(src, 'lxml')
+
+all_products_hrefs = soup.find_all(class_='mzr-tc-group-item-href')
+all_category_dict = {}
+for item in all_products_hrefs:
+    # print(item)
+    item_text = item.text
+    item_href = "https://health-diet.ru" + item.get("href")
+    # print(f"{item_text}:{item_href}")
+    all_category_dict[item_text] = item_href
+
+with open("all_category_dict.json", 'w') as file:
+    json.dump(all_category_dict, file, indent=4, ensure_ascii=False)
