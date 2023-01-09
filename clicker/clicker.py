7a4f0da409a73eff820156b4d487c2dd4aaa5ab1
2pokek <alkuzmich6479@gmail.com>
2023-01-09 23:45:13 +0300

    clicker 2

--- clicker/clicker.py
+++ clicker/clicker.py
@@ -8,12 +8,21 @@ clicking = False
 mouse = Controller
 
 def clicker():
-    opass
+    while True:
+        if clicking:
+            mouse.click(Button.left,1)
+            time.sleep(0.1)
 
-def toggle_ivent():
-    pass
+def toggle_ivent(key):
+    if key == toggle_key:
+        global clicking
+        clicking = not clicking
 
 def main():
-    pass
+    clicking_thread = threading.Thread(target=clicker)
+    clicking_thread.start()
+
+    with Listener(on_press=toggle_event) as listener:
+        listener.join()
 if __name__='__main__':
     main()
