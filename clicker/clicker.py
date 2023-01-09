e85053ab3e405c7d000486b3227791da97342ce1
2pokek <alkuzmich6479@gmail.com>
2023-01-09 23:41:46 +0300

    clicker1

---
+++ clicker/clicker.py
@@ -0,0 +1,19 @@
+import time
+import threading
+from pynput.mouse import Controller, Button
+from pynput.keyboard import Listener, KeyCode
+
+toggle_key = KeyCode(char='p')
+clicking = False
+mouse = Controller
+
+def clicker():
+    opass
+
+def toggle_ivent():
+    pass
+
+def main():
+    pass
+if __name__='__main__':
+    main()
